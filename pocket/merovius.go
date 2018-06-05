42241bfb0edf5e88ccb9fbfa1440de10a4e65c86
hezhizhen <hezhizhen.yi@gmail.com>
2018-06-05 11:32:18 +0800

    add https://blog.merovius.de

---
+++ pocket/merovius.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func handleMerovius(url string, p Pocket) {
+	var urls []string
+	doc, err := goquery.NewDocument(url)
+	handleError(err)
+	list := doc.Find("div.col.s12.m12.l6.xl4")
+	list.Each(func(i int, s *goquery.Selection) {
+		href, exist := s.Find("a.black-text").Attr("href")
+		if !exist {
+			panic("missing url")
+		}
+		urls = append(urls, url+href)
+	})
+	p.AddMultiple(urls)
+	for i := range urls {
+		fmt.Printf("%2d: %s\n", i+1, urls[i])
+	}
+}
